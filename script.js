57e6d759d0902f7eb3b883592a6871ea76c0948d
Loucas Ioannou <149908533+loucas999999@users.noreply.github.com>
2024-04-29 23:03:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el)); 
 
-// Services Scroll Animation
+// Services Scroll To Section Animation
 
 function scrollToElement(elementSelector, instance = 0){
     const elements = document.querySelectorAll(elementSelector);
@@ -24,58 +24,17 @@ function scrollToElement(elementSelector, instance = 0){
 }
 
 const link1 = document.querySelector('#test');
+const link2 = document.querySelector('.container__btn2');
 
 link1.addEventListener('click', () => {
     scrollToElement('.second-section');
 })
 
-// HOVER OVER BOXES OPACITY 
-
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-const box3 = document.querySelector('.box3');
-const box4 = document.querySelector('.box4');
-
-  box1.addEventListener('mouseenter', () => box2.style.opacity = '0.5');
-  box1.addEventListener('mouseenter', () => box3.style.opacity = '0.5');
-  box1.addEventListener('mouseenter', () => box4.style.opacity = '0.5');
-  box1.addEventListener('mouseleave', () => box2.style.opacity = '1');
-  box1.addEventListener('mouseleave', () => box3.style.opacity = '1');
-  box1.addEventListener('mouseleave', () => box4.style.opacity = '1');
-
-  box2.addEventListener('mouseenter', () => box1.style.opacity = '0.5');
-  box2.addEventListener('mouseenter', () => box3.style.opacity = '0.5');
-  box2.addEventListener('mouseenter', () => box4.style.opacity = '0.5');
-  box2.addEventListener('mouseleave', () => box1.style.opacity = '1');
-  box2.addEventListener('mouseleave', () => box3.style.opacity = '1');
-  box2.addEventListener('mouseleave', () => box4.style.opacity = '1');
-
-  box3.addEventListener('mouseenter', () => box1.style.opacity = '0.5');
-  box3.addEventListener('mouseenter', () => box2.style.opacity = '0.5');
-  box3.addEventListener('mouseenter', () => box4.style.opacity = '0.5');
-  box3.addEventListener('mouseleave', () => box1.style.opacity = '1');
-  box3.addEventListener('mouseleave', () => box2.style.opacity = '1');
-  box3.addEventListener('mouseleave', () => box4.style.opacity = '1');
-
-  box4.addEventListener('mouseenter', () => box1.style.opacity = '0.5');
-  box4.addEventListener('mouseenter', () => box3.style.opacity = '0.5');
-  box4.addEventListener('mouseenter', () => box2.style.opacity = '0.5');
-  box4.addEventListener('mouseleave', () => box1.style.opacity = '1');
-  box4.addEventListener('mouseleave', () => box3.style.opacity = '1');
-  box4.addEventListener('mouseleave', () => box2.style.opacity = '1');
+link2.addEventListener('click', () => {
+    scrollToElement('.second-section');
+})
 
 // FREQUENTLY ASKED QUESTIONS 
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const questions = document.querySelectorAll('.question');
-  
-//     questions.forEach(function(question) {
-//       question.addEventListener('click', function() {
-//         const answer = this.nextElementSibling
-//         answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-//       });
-//     });
-//   });
 
 const faqs = document.querySelectorAll('.faq');
 
